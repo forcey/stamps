ffc30142d751d2e0e8d50782804f5da27549d8de
Todd Li <forcey@gmail.com>
2022-12-17 21:28:58 -0800

    hook up with algorithm

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import calculate from './algorithm/calculate';
+import React, { useState } from 'react';
 
 function App() {
   const stamps = [1, 3, 5, 10, 33, "forever", 65, 86, "global forever"];
+  const [selected, setSelected] = useState(new Set());
+  const [target, setTarget] = useState(0);
 
   const onSelectionChanged = function (selected) {
-    console.log("Selected: " + Array.from(selected).join(", "));
+    setSelected(selected);
+  }
+
+  const onCalculate = function () {
+    const values = Array.from(selected).map(function (value) {
+      if (value === "forever") {
+        return 60;
+      } else if (value === "global forever") {
+        return 140;
+      } else {
+        return value;
+      }
+    });
+    const solution = calculate(values, target);
+    console.log(solution);
   }
 
   return (
@@ -22,10 +40,10 @@ function App() {
         <Row>
           <Col sm>
             <Form.Label htmlFor="target">Need</Form.Label>
-            <Form.Control id="target" type="number" placeholder="100" htmlSize="10" />
+            <Form.Control id="target" type="number" placeholder="100" htmlSize="10" onChange={e => setTarget(e.target.value)} />
           </Col>
           <Col sm>
-            <Button>Calculate</Button>
+            <Button onClick={onCalculate}>Calculate</Button>
           </Col>
         </Row>
       </Container>
---
+++ src/algorithm/calculate.js
@@ -0,0 +1,6 @@
+function calculate(stamps, target) {
+    console.log(stamps, target);
+    return [];
+}
+
+export default calculate;
